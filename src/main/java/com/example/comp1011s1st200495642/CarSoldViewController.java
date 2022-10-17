0ebc88daa11c48e55a2e5ec31ea72dce1b8db204
aman maan <200495642@student.georgianc.on.ca>
2022-10-17 13:48:27 -0400

    3a complete

--- src/main/java/com/example/comp1011s1st200495642/CarSoldViewController.java
+++ src/main/java/com/example/comp1011s1st200495642/CarSoldViewController.java
@@ -1,44 +1,51 @@
 package com.example.comp1011s1st200495642;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class CarSoldViewController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class CarSoldViewController implements Initializable {
 
     @FXML
     private BarChart<?, ?> barChart;
 
     @FXML
-    private TableColumn<?, ?> fifthColumnPrice;
+    private TableColumn<CarSold, Integer> fifthColumnPrice;
 
     @FXML
-    private ComboBox<?> filterByYear;
+    private ComboBox<CarSold> filterByYear;
 
     @FXML
-    private TableColumn<?, ?> firstColumnCarId;
+    private TableColumn<CarSold, Integer> firstColumnCarId;
 
     @FXML
-    private TableColumn<?, ?> fourthModel;
+    private TableColumn<CarSold, String> fourthModel;
 
     @FXML
     private CategoryAxis horizontalAxis;
 
     @FXML
-    private TableColumn<?, ?> lastCoulmnDateSold;
+    private TableColumn<CarSold, Integer> lastCoulmnDateSold;
 
     @FXML
     private NumberAxis numberAxis;
 
     @FXML
-    private TableColumn<?, ?> secondColumnModelYear;
+    private TableColumn<CarSold, Integer> secondColumnModelYear;
 
     @FXML
-    private TableColumn<?, ?> thirdColumnMake;
+    private TableColumn<CarSold, String> thirdColumnMake;
 
     @FXML
     private Label totalSalesLabel;
@@ -46,4 +53,22 @@ public class CarSoldViewController {
     @FXML
     private Label unitsLabel;
 
+
+    @FXML
+    private TableView<CarSold> tableView;
+
+    @Override
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        firstColumnCarId.setCellValueFactory(new PropertyValueFactory<>("carId"));
+        secondColumnModelYear.setCellValueFactory(new PropertyValueFactory<>("modelYear"));
+        thirdColumnMake.setCellValueFactory(new PropertyValueFactory<>("make"));
+        fourthModel.setCellValueFactory(new PropertyValueFactory<>("model"));
+        fifthColumnPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        lastCoulmnDateSold.setCellValueFactory(new PropertyValueFactory<>("dateSold"));
+
+        ArrayList<CarSold> carSolds = DBUtilities.getSoldCarObjectFromDB();
+        tableView.getItems().addAll(DBUtilities.getSoldCarObjectFromDB());
+    }
 }
+
